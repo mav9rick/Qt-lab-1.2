29a5b4a877dda1d5e94d179d9dc40a09e76e8d74
mav9rick <koil-3@yandex.ru>
2024-05-01 15:25:52 +0500

    Изменена реализация main.cpp

--- FileManager/main.cpp
+++ FileManager/main.cpp
@@ -1,28 +1,15 @@
-#include <iostream>
-#include <string>
-#include <map>
+#include "win.h"
 #include <locale>
 #include <logger.h>
 #include <file.h>
 #include <QTextStream>
 #include <QDir>
 using namespace std;
-
-int main()
+int main(int argc, char *argv[])
 {
-    locale::global( locale("ru_RU.UTF-8"));
-    logger a;
-    while (true)
-    {
-        QString b = a.getstring();
-        if (b != "/exit")
-        {
-            a.runcommand(b);
-        }
-        else
-        {
-            return -1;
-        }
-    }
-    return 0;
+    //setlocale(LC_ALL, "Russian");
+    locale::global(locale("ru_RU.UTF-8"));
+    win a(argc, argv);
+    return a.exec();
 }
+
